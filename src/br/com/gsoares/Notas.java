3bdbdde6b8e0c053d6f40c8bb653dba06e65dddb
gabrielluiz299 <gabrielluiz299@gmail.com>
2024-08-12 22:29:00 -0300

    projetos de medias de notas envolvendo operações numericas

---
+++ src/br/com/gsoares/Notas.java
@@ -0,0 +1,14 @@
+package br.com.gsoares;
+
+public class Notas {
+	public static void main(String[] args) {
+	int notaUm=10;
+	int notaDois=5;
+	int notaTres=8;
+	int notaQuatro=9;
+	
+	double media = (notaUm+notaDois+notaTres+notaQuatro) / 4;
+	
+		System.out.print(media);
+	}
+}
